ae11bc122fc7dba732e2e069dfcc21c8b6120391
Mike Henke <henke.mike@gmail.com>
2023-05-10 19:35:07 -0500

    first attempt

--- src/KeyboardHelperExtension.ts
+++ src/KeyboardHelperExtension.ts
@@ -16,26 +16,27 @@ export class KeyboardHelperExtension implements vscode.Disposable {
   private allShortcuts: IShortCut[] = [];
 
   private _disposable: vscode.Disposable[] = [];
-  private statusBarItem: vscode.StatusBarItem;
+  private statusBarItem: vscode.MarkdownString;
+
 
   public constructor(private readonly context: vscode.ExtensionContext) {
     this.allShortcuts = this.getAllShortcuts();
     this.currentIndex = this.context.globalState.get(CURRENT_INDEX_KEY, 0);
     this.currentShortcut = this.getCurrentShortcut();
     this.statusBarItem = this.createAndShowStatusBarItem();
-
-    this.refreshStatusBar();
-
+  
+    this.addStatusBarItem();
+  
     const showMoreInformationCommand = vscode.commands.registerCommand(
       "learn-keyboard-shortcuts.showMoreInformation",
-      () => this.showMoreInformation()
+      () => this.selectShortcut()
     );
-
+  
     const selectShortcut = vscode.commands.registerCommand(
       "learn-keyboard-shortcuts.selectShortcut",
       () => this.selectShortcut()
     );
-
+  
     // When the index changes update the displayed shortcut and save to global state
     this.shortCutIndexEvent.event((index: number) => {
       this.currentIndex = index;
@@ -43,42 +44,55 @@ export class KeyboardHelperExtension implements vscode.Disposable {
       this.refreshStatusBar();
       context.globalState.update(CURRENT_INDEX_KEY, this.currentIndex);
     });
-
+  
     this._disposable.push(showMoreInformationCommand);
-    this._disposable.push(this.statusBarItem);
-    this._disposable.push(this.shortCutIndexEvent);
     this._disposable.push(selectShortcut);
+    this._disposable.push(this.shortCutIndexEvent);
+  }  
+
+  private showShortcutInfo(shortcut: IShortCut) {
+    const markdownString = new vscode.MarkdownString();
+    markdownString.appendMarkdown(`**${shortcut.keys}**: ${shortcut.text}`);
+    this.statusBarItem.value = markdownString;
+  }
+  
+  private onDidChangeTextEditorSelection(event: vscode.TextEditorSelectionChangeEvent) {
+    const selection = event.selections[0];
+    const editor = vscode.window.activeTextEditor;
+    const line = editor.document.lineAt(selection.active.line);
+  
+    // Check if the selection is within a certain range (e.g., one line)
+    if (selection.isEmpty && selection.active.character === line.range.end.character) {
+      const shortcut = this.getShortcutForAction(line.text.trim());
+      if (shortcut) {
+        this.showShortcutInfo(shortcut);
+        return;
+      }
+    }
+  
+    // If there is no relevant shortcut, clear the status bar item
+    this.statusBarItem.value = "";
   }
 
-  private async showMoreInformation() {
-    const result = await vscode.window.showInformationMessage(
-      `${this.currentShortcut.keys}: ${this.currentShortcut.text}`,
-      "Choose Shortcut",
-      "Previous",
-      "Next"
-    );
-
-    if (result === "Choose Shortcut") {
-      this.selectShortcut();
-    }
-
-    if (result === "Previous") {
-      this.setPreviousShortcut();
-    }
-
-    if (result === "Next") {
-      this.setNextShortcut();
-    }
+  private addStatusBarItem() {
+    vscode.window.onDidChangeTextEditorSelection(this.onDidChangeTextEditorSelection, this, this._disposable);
+    vscode.window.onDidChangeActiveTextEditor(() => this.statusBarItem.value = "", this, this._disposable);
+    vscode.window.onDidChangeTextEditorVisibleRanges(() => this.statusBarItem.value = "", this, this._disposable);
+    this.statusBarItem.show();
   }
+  
+  private getShortcutForAction(action: string): IShortCut {
+    return this.allShortcuts.find((shortcut) => {
+      const regex = new RegExp(`\\b${shortcut.keys}\\b`, "i");
+      return regex.test(action);
+    });
+  }
+  
 
-  private createAndShowStatusBarItem(): vscode.StatusBarItem {
-    const statusBarItem = vscode.window.createStatusBarItem(
-      vscode.StatusBarAlignment.Right,
-      0
-    );
-    statusBarItem.command = "learn-keyboard-shortcuts.showMoreInformation";
-    statusBarItem.show();
-    return statusBarItem;
+  private createAndShowStatusBarItem(): vscode.MarkdownString {
+    const markdownString = new vscode.MarkdownString();
+    markdownString.isTrusted = true;
+    return markdownString;
   }
 
   private refreshStatusBar() {
